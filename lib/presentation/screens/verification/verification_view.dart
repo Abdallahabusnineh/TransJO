056470d4fb62cc4f5c519f6c4ef8a018c5a0ae6c
abdallah abusnineh <abooodabusnineh2002@gmail.com>
2024-05-10 17:26:48 +0300

    Verification bloc is done

--- lib/presentation/screens/verification/verification_view.dart
+++ lib/presentation/screens/verification/verification_view.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:transjo/core/common_widgets/navigations_types.dart';
+import 'package:transjo/core/common_widgets/show_toast.dart';
 import 'package:transjo/core/services/services_locater.dart';
 import 'package:transjo/presentation/blocs/verification_bloc.dart';
 import 'package:transjo/presentation/screens/password_change_isdone/password_change_view.dart';
 import 'package:transjo/presentation/screens/verification/verification_content.dart';
-
 class VerificationView extends StatelessWidget {
   const VerificationView({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -17,8 +16,12 @@ class VerificationView extends StatelessWidget {
         listener: (context, state) {
       if(state is VerificationSuccessState) {
         navigateTo(context, PasswordChangeView());
+        showToast(text: 'change password successfully', state: ToastState.SUCCESS);
       }
-        },
+      else if (state is VerificationServerFailureState || state is VerificationErrorState  )
+        showToast(text: 'your code is not correct', state: ToastState.ERROR);
+      },
+
         child: Scaffold(
           body: VerificationContent(),
         ),
