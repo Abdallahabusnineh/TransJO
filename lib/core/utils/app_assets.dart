8fe5e65e770c7a98ba759f871a72cf4b339853d2
abdallah abusnineh <abooodabusnineh2002@gmail.com>
2024-05-18 00:07:37 +0300

    commit

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -5,12 +5,7 @@ abstract class AppAssets {
   static const String loginBus="${imageBase}Animation - 1702035910937.json";
   static const String googleIcon="${imageBase}google.png";
   static const String cameraIcon="${imageBase}diaphragm.png";
-   static  String emailAuth="";
-   static  String passwordAuth="";
-   /*static String basicAuth='Basic ' + base64.encode(utf8.encode('$emailAuth:$passwordAuth'));*/
-  static String basicAuth =
-      'Basic ${base64.encode(utf8.encode('{${emailAuth}}-{${passwordAuth}}'))}';
-  /*static String basicAuth =
+   /*static String basicAuth =
       'Basic ${base64.encode(utf8.encode('11165489:60-dayfreetrial'))}';
 */
 }
